813284ef0485c88357f078fb310eb5a40d7a4f29
joshpson <jpearson846@gmail.com>
2018-06-27 10:16:13 -0400

    Done.

--- src/containers/MasterHog.js
+++ src/containers/MasterHog.js
@@ -1,68 +1,84 @@
-import React, { Component } from 'react'
-import { Button, Checkbox, Form, Input, Radio, Select, TextArea } from 'semantic-ui-react'
-import Master from '../assets/master-hog.png'
-import BabyHog from '../components/BabyHog'
+import React, { Component } from "react";
+import {
+  Button,
+  Checkbox,
+  Form,
+  Input,
+  Radio,
+  Select,
+  TextArea
+} from "semantic-ui-react";
+import Master from "../assets/master-hog.png";
+import BabyHog from "../components/BabyHog";
 
 class MasterHog extends Component {
-
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
       eyeColor: "blue",
       name: "Master Blaster",
-      weight: '2.54 Tons',
+      weight: "2.54 Tons",
       offspring: []
-    }
+    };
   }
-
 
   changeEyeColor(event) {
-    event.preventDefault()
-
+    event.preventDefault();
+    this.setState({
+      eyeColor: event.target.value
+    });
   }
-
 
   render() {
     return (
       <div>
-
         <form>
           <input
             type="radio"
             name="eyeColor"
             value="blue"
-            onChange={this.changeEyeColor.bind(this)}>
-          </input> Blue<br></br>
+            onChange={this.changeEyeColor.bind(this)}
+          />{" "}
+          Blue<br />
           <input
             type="radio"
             name="eyeColor"
             value="sun"
-            onChange={this.changeEyeColor.bind(this)}>
-          </input> Sun<br></br>
+            onChange={this.changeEyeColor.bind(this)}
+          />{" "}
+          Sun<br />
           <input
             type="radio"
             name="eyeColor"
             value="glowing"
-            onChange={this.changeEyeColor.bind(this)}>
-          </input> Glowing<br></br>
+            onChange={this.changeEyeColor.bind(this)}
+          />{" "}
+          Glowing<br />
         </form>
-
 
         <h2>Name: {this.state.name}</h2>
         <h3>Weight: {this.state.weight}</h3>
         <h3>Eye Color: {this.state.eyeColor}</h3>
         <div id="masters-domicile">
-          <img id="master-blaster" src={Master} alt='MasterBlaster' style={{height: 400}}></img>
+          <img
+            id="master-blaster"
+            src={Master}
+            alt="MasterBlaster"
+            style={{ height: 400 }}
+          />
         </div>
         <ul className="hoglist">
-          {/* render hog babies */}
+          {this.props.offspring.map(baby => (
+            <BabyHog
+              name={baby.name}
+              weight={baby.weight}
+              eyeColor={this.state.eyeColor}
+            />
+          ))}
         </ul>
-
       </div>
-
-    )
+    );
   }
-
 }
 
 export default MasterHog;
